3f148e9e362c9119d33c5676218165fc97f1ee60
triggor <m13igor@gmail.com>
2018-02-25 17:15:27 +0100

    Add files via upload

---
+++ 5.semester/Embedded_Systems/Solutions/Lista6/RFC_receiver.ino
@@ -0,0 +1,27 @@
+ #include <SPI.h>
+ #include "RF24.h"
+ //SCK -> 13//MISO -> 12//MOSI -> 11//CSN -> 7//CE -> 8
+RF24 radio(8,7);
+ const uint64_t pipe = 0xE8E8F0F0E1LL; 
+ int msg[1];
+ void setup() 
+ { 
+ Serial.begin(9600);
+ radio.begin();
+ radio.openReadingPipe(1,pipe);
+ radio.startListening(); 
+ } 
+ void loop() 
+ { 
+  Serial.print ("fff");
+ if (radio.available()){
+  Serial.print ("ggg");
+ bool done = false;
+ while (!done){
+ done = radio.read(msg, 1); 
+ Serial.print ("gaga");
+ Serial.print (msg[0]);
+  
+ }
+ }
+ }
---
+++ 5.semester/Embedded_Systems/Solutions/Lista6/RFC_transmitter.ino
@@ -0,0 +1,19 @@
+#include <SPI.h>
+#include "RF24.h"
+int msg[1];
+//SCK -> 13//MISO -> 12//MOSI -> 11//CSN -> 7//CE -> 8
+RF24 radio(8,7);
+const uint64_t pipe = 0xE8E8F0F0E1LL; 
+int potpin = 0; 
+int val; 
+void setup(void){ 
+ radio.begin();
+ radio.openWritingPipe(pipe); 
+}
+void loop(void){ 
+ val = 5; 
+  
+ msg[0] = val;
+ radio.write(msg, 1);
+ delay(1000);
+}
